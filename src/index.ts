f3afbdb6fad9d609fdec8417ed27e23043aba926
Gadi Piperno Corcos <gadi@skyriseltd.com>
2023-05-19 19:08:26 +0200

    Changed the example to the test the error being triggered by recursive queue message send operations

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,72 @@
 export interface Env {
   DATA: KVNamespace
-  QUEUE: Queue<number>
+  QUEUE: Queue<{ depthToReach: number; currentDepth: number }>
 }
 
-let kvCallsCount = 0
+const MESSAGES_TO_ENQUEUE = 30
+const DEPTH_TO_REACH = 3
+
+let processedMessagesCount = 0
+let activeMessagesCount = 0
+let totalDepth = 0
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const text = await request.text()
-    let n = parseInt(text) || 0
-    if (n <= 0) n = 10
-    if (n > 100) n = 100
+    let depthToReach = (await getIntFromRequest(request, DEPTH_TO_REACH)) - 1
 
-    console.log(`[fetch] messages to be processed: ${n}`)
-    await env.QUEUE.send(n)
+    console.log(`[fetch] enqueuing ${MESSAGES_TO_ENQUEUE} messages with depthToReach=${depthToReach}`)
+    for (let i = 0; i < MESSAGES_TO_ENQUEUE; i++) {
+      await env.QUEUE.send({ depthToReach, currentDepth: 0 })
+    }
     return new Response('ok')
   },
 
   async queue(batch: MessageBatch<any>, env: Env, executionCtx: ExecutionContext) {
     for (let message of batch.messages) {
+      activeMessagesCount++
+
+      const { depthToReach, currentDepth } = message.body
+      const remainingDepth = depthToReach - currentDepth
+
+      const log = (text = '') => {
+        console.log(`[queue message ${message.id}] ${text} (depth: ${currentDepth})`)
+      }
+
       try {
         await env.DATA.get('foo')
-        kvCallsCount++
-        await env.DATA.get('foo')
-        kvCallsCount++
-        await env.DATA.get('foo')
-        kvCallsCount++
 
-        if (message.body > 0) {
-          console.log(`[queue message ${message.id}], messages to be processed: ${message.body - 1},  kv ops count: ${kvCallsCount}`)
-          await env.QUEUE.send(message.body - 1)
+        if (remainingDepth > 0) {
+          log(`remaining depth: ${remainingDepth},`)
+          await env.QUEUE.send({ depthToReach, currentDepth: currentDepth + 1 })
+          if (currentDepth > 1) totalDepth++
         } else {
-          console.log(`[queue message ${message.id}], no more messages to process, kv ops count: ${kvCallsCount}`)
+          log(`reached depth ${depthToReach}: stopping`)
+          if (currentDepth > 1) totalDepth -= depthToReach - 2
         }
+
+        activeMessagesCount--
+        processedMessagesCount++
       } catch (e: any) {
-        console.error(e?.stack || e, `kv ops count: ${kvCallsCount}`)
+        console.error(
+          `[queue message ${
+            message.id
+          } Exception] (depth: ${currentDepth}, totalDepth: ${totalDepth}, processed messages: ${processedMessagesCount}, currently active messages in this instance: ${activeMessagesCount})\n${
+            e?.stack || e
+          }`
+        )
+        processedMessagesCount++
+        activeMessagesCount--
         throw e
       }
     }
   },
+}
+
+async function getIntFromRequest(req: Request, defaultValue: number) {
+  if (defaultValue <= 0) throw new Error('default value must be greater than 0')
+  const text = await req.text()
+  let n = parseInt(text) || 0
+  if (n <= 0) n = defaultValue
+  if (n > 100) n = 100
+  return n
 }
